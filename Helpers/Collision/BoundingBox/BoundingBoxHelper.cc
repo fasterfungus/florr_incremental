dd5a61d593ce5360690b107f9d30f767bdb25c3d
fasterfungus <3149015050@qq.com>
2026-07-19 17:58:47 +0800

    将碰撞算法修改为bvh

--- Helpers/Collision/BoundingBox/BoundingBoxHelper.cc
+++ Helpers/Collision/BoundingBox/BoundingBoxHelper.cc
@@ -4,43 +4,16 @@
 #include <Helpers/Vector.hh>
 #include <Shared/Entity.hh>
 
+#include "Helpers/Collision/GeometryHelper.hh"
+
 void Set(Entity& e)
 {
-    Vector p1 = GetFarthestProjectionPoint(e, up);
-    Vector p2 = GetFarthestProjectionPoint(e, right);
-    Vector p3 = GetFarthestProjectionPoint(e, down);
-    Vector p4 = GetFarthestProjectionPoint(e, left);
-    e.set_maxx(Max(p1.x, p2.x, p3.x, p4.x) * e.get_scale());
-    e.set_maxy(Max(p1.y, p2.y, p3.y, p4.y) * e.get_scale());
-    e.set_minx(Min(p1.x, p2.x, p3.x, p4.x) * e.get_scale());
-    e.set_miny(Min(p1.y, p2.y, p3.y, p4.y) * e.get_scale());
-}
-
-bool BoundingBoxContains(const Entity& e, const Vector& pt)
-{
-    float minx = e.get_minx() + e.get_x();
-    float miny = e.get_miny() + e.get_y();
-    float maxx = e.get_maxx() + e.get_x();
-    float maxy = e.get_maxy() + e.get_y();
-
-    return minx <= pt.x && maxx >= pt.x &&
-        miny <= pt.y && maxy >= pt.y;
-}
-
-bool Overlaps(const Entity& e1, const Entity& e2)
-{
-    float minx1 = e1.get_minx() + e1.get_x();
-    float miny1 = e1.get_miny() + e1.get_y();
-    float maxx1 = e1.get_maxx() + e1.get_x();
-    float maxy1 = e1.get_maxy() + e1.get_y();
-
-    float minx2 = e2.get_minx() + e2.get_x();
-    float miny2 = e2.get_miny() + e2.get_y();
-    float maxx2 = e2.get_maxx() + e2.get_x();
-    float maxy2 = e2.get_maxy() + e2.get_y();
-
-    if (minx1 > maxx2 || maxx1 < minx2) return false;
-    if (miny1 > maxy2 || maxy1 < miny2) return false;
-
-    return true;
+    Vector p1 = GetFarthestProjectionPointAfterRotation(e, up);
+    Vector p2 = GetFarthestProjectionPointAfterRotation(e, right);
+    Vector p3 = GetFarthestProjectionPointAfterRotation(e, down);
+    Vector p4 = GetFarthestProjectionPointAfterRotation(e, left);
+    e.set_maxx(Max(p1.x, p2.x, p3.x, p4.x));
+    e.set_maxy(Max(p1.y, p2.y, p3.y, p4.y));
+    e.set_minx(Min(p1.x, p2.x, p3.x, p4.x));
+    e.set_miny(Min(p1.y, p2.y, p3.y, p4.y));
 }
--- Helpers/Collision/BoundingBox/BoundingBoxHelper.hh
+++ Helpers/Collision/BoundingBox/BoundingBoxHelper.hh
@@ -1,5 +1,3 @@
 #pragma once
 #include <Shared/Entity.hh>
 void Set(Entity & e);
-bool BoundingBoxContains(const Entity& e, const Vector& pt);
-bool Overlaps(const Entity& e1, const Entity& e2);
