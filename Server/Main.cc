fdbbee64a3192f9c2f0a4fbfffbf824b0fe8acc5
fasterfungus <3149015050@qq.com>
2026-08-13 18:43:32 +0800

    fix

--- Server/Main.cc
+++ Server/Main.cc
@@ -4,16 +4,6 @@
 #include <iostream>
 
 int main() {
-    //test_aabb_helpers();
-    //test_edge_cases();
-    //test_move_noop_when_inside_fat();
-    //test_persistent_lifecycle();
-    //test_tree_quality();
-    //test_cross_tree_pairs();
-    //test_ccd_sweep();
-    //test_ccd_segment();
-    //if (test_superset_scenes() != 0) return 1;
-    std::printf("ALL BVH TESTS PASSED\n");
     std::cout << "Diagnostics: {\n";
     std::cout << "  Simulation Size: " << sizeof(Simulation) << '\n';
     std::cout << "  Entity Size: " << sizeof(Entity) << '\n';
